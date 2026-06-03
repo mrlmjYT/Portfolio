e1db4a71291b516d09e37d7490bd64808c76a6da
mrlmjYT <mr.lmj.email@gmail.com>
2026-06-03 10:46:56 +0200

    updated all
    i updated all of the files

---
+++ script.js
@@ -0,0 +1,19 @@
+function showProjects(){
+    document.body.innerHTML += 
+    `
+    <div id="projects">
+        <h1>Projects</h1>
+        <p>Here you can find some of my projects.</p>
+    </div>
+    `
+}
+
+function showInfo(){
+    document.body.innerHTML += 
+    `
+    <div id="info">
+        <h1>Info</h1>
+        <p>Here you can find some information about me.</p>
+    </div>
+    `
+}
